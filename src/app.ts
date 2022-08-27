6dbf57c3b83dd9ed54c7302a26ee5b30e7030b02
Kahmyl <kamilhabib556@gmail.com>
2022-08-27 15:52:18 -0700

    setting up QuotaGuard IP

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,12 @@ dotenv.config();
 
 const app = express();
 
+var http, options, proxy, url, target;
+
+http = require("http");
+
+url = require("url");
+
 const port = process.env.PORT ? parseInt(process.env.PORT) : 8500;
 const host = process.env.HOST ? process.env.HOST : "localhost";
 
@@ -32,6 +38,24 @@ app.use(deserializeUser);
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/v1/", router);
+
+proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
+target = url.parse("http://ip.quotaguard.com/");
+
+options = {
+  hostname: proxy.hostname,
+  port: proxy.port,
+  path: target.href,
+  headers: {
+    "Proxy-Authorization": "Basic " + new Buffer(proxy.auth).toString("base64"),
+    Host: target.hostname,
+  },
+};
+
+http.get(options, function (res: any) {
+  res.pipe(process.stdout);
+  // return console.log("status code", res.statusCode);
+});
 
 app.listen(port, async () => {
   log.info(`app is listening at http://${host}:${port}`);
